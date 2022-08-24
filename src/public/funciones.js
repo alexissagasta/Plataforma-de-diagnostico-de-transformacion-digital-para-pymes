90e533244daa220a28423572ebea2c008e4e3d48
Alexis Issac Sagasta Ontiveros <alexissagasta@gmail.com>
2022-08-24 12:01:42 -0700

    Se agrego la habilidad de descargar pdf

--- src/public/funciones.js
+++ src/public/funciones.js
@@ -297,7 +297,7 @@ async function evaluar() {
       "descripcion": nivel.descripcion,
       "rango": (nivel.nivel + ": " + nivel.rango)
     }
-
+    document.getElementById('divPDF').style.display  = 'block';
     console.log(evaluacion)
     await guardarEvaluacion(evaluacion);
   } else {
@@ -361,8 +361,37 @@ async function evaluar() {
         "descripcion": nivel.descripcion,
         "rango": (nivel.nivel + ": " + nivel.rango)
       }
-      await guardarEvaluacion(evaluacion);
+      await guardarEvaluacion(evaluacion); 
     }
   }
+}
+async function createPDF() {
+  let empresaRaw = document.getElementById('idEmpresa').textContent;
+  let empresa = empresaRaw.replace(/\s+/g, '');
+  var sTable = document.getElementById('ev').innerHTML;
+  var sDesc = document.getElementById('desc').innerHTML;
+  
+  var style = "<style>";
+  style = style + "table {width: 100%;font: 17px Calibri;}";
+  style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+  style = style + "padding: 2px 3px;text-align: center;}";
+  style = style + "</style>";
+
+  // CREATE A WINDOW OBJECT.
+  var win = window.open('', '', 'height=700,width=700');
+
+  win.document.write('<html><head>');
+  win.document.write('<h1 style="text-align: center;">'+empresa+'</h1>');
+  win.document.write('<title>Calculo</title>');   // <title> FOR PDF HEADER.
+  win.document.write(style);          // ADD STYLE INSIDE THE HEAD TAG.
+  win.document.write('</head>');
+  win.document.write('<body>');
+  win.document.write(sTable);         // THE TABLE CONTENTS INSIDE THE BODY TAG.
+  win.document.write(sDesc);
+  win.document.write('</body></html>');
+
+  win.document.close(); 	// CLOSE THE CURRENT WINDOW.
+
+  win.print();    // PRINT THE CONTENTS.
 }
 
